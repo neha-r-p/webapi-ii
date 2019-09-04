00dc5e3b62a5936b3b28f08037eb3f19ed9ebfbc
Neha Patel <neha.r.p@gmail.com>
2019-09-03 21:28:08 -0700

    building out put request

--- index.js
+++ index.js
@@ -119,6 +119,39 @@ server.delete("/api/posts/:id", (req, res) => {
 });
 
 // update a post with PUT request
+server.put('/api/posts/:id', (req, res) => {
+    const changes = req.body;
+    const postId = req.params.id;
+    Posts.update(postId, changes)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch()
+})
+
+
+// server.put('/api/hubs/:id', (req, res) => {
+//     const changes = req.body;
+//     Hubs.update(req.params.id, changes)
+//     .then(hub => {
+//       if (hub) {
+//         res.status(200).json(hub);
+//       } else {
+//         res.status(404).json({ message: 'The hub could not be found' });
+//       }
+//     })
+//     .catch(error => {
+//       // log error to database
+//       console.log(error);
+//       res.status(500).json({
+//         message: 'Error updating the hub',
+//       });
+//     });
+//   });
 
 const port = 6666;
 server.listen(port, () => console.log(`\napi running on port ${port}\n`));
